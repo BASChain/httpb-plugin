751a6ef41fc6fac998f5b8073bc1b7ca46ed2818
bas-devteam@gmail.com <front-devgmail.com>
2019-12-24 13:57:27 +0800

    publish 0.0.4

---
+++ .config/web-ext-config.js
@@ -0,0 +1,20 @@
+const pkgJson = require('../package.json')
+
+const ExtensionName = pkgJson.name ||'basexer'
+var pkgPathName = ExtensionName
+
+if(pkgJson.version && pkgJson.version.split('.').length>0){
+  pkgPathName = pkgPathName + '_' + pkgJson.version.split('.').join('_')
+}
+
+module.exports = {
+  verbose: true,
+  build: {
+    overwriteDest: true
+  },
+  run: {
+
+  },
+  artifactsDir:"dist/firefox",
+  sourceDir: `build/${pkgPathName}_firefox`
+}
---
+++ docs/firefox_install_wiki.md
@@ -0,0 +1,9 @@
+# Firefox Browser Usage
+
+> This Extension [Basexer] can support blockchain domain name resolution, identified by the httpb protocol
+
+>The extension requires firefox minimum version 68.0 
+
+## Download Basexer 
+
+![]()

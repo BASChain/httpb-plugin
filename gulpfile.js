14a5c54cdbf0a66492deffcbe6958dc454927bb7
bas-devteam@gmail.com <front-devgmail.com>
2020-01-14 12:39:06 +0800

    review firefox

--- gulpfile.js
+++ gulpfile.js
@@ -1,315 +1,245 @@
-const gulp = require('gulp'),
-  packageJson = require('./package.json'),
-  autoprefixer = require('gulp-autoprefixer'),
-  cssnano = require('cssnano'),
-  del = require("del"),
+/**
+ *     ____  ___   _____
+ *    / __ )/   | / ___/
+ *   / __  / /| | \__ \
+ *  / /_/ / ___ |___/ /
+ * /_____/_/  |_/____/
+ *
+ * Copyright (c) 2020 BAS,bas-devteam@gmail.com
+ * E-mail :front-devgmail.com
+ * git@flash:BASChain/httpb-plugin.git
+ *
+ */
+'use strict'
+
+const pkgJson = require('./package.json'),
+  projectJson = require('./.config/project.json'),
+  assign = require('lodash.assign'),
+  browserify = require('browserify'),
   buffer = require('vinyl-buffer'),
+  chmod = require('gulp-chmod'),
+  DateFormat = require('fast-date-format'),
+  del = require('del'),
+  dotenv = require('dotenv'),
+  envify = require('envify/custom'),
+  gulp = require('gulp'),
   gutil = require('gulp-util'),
-  gulpMultiProcess = require('gulp-multi-process'),
+  jsoneditor = require('gulp-json-editor'),
   livereload = require('gulp-livereload'),
-  jsoneditor = require('gulp-json-editor')
-  postcss = require('gulp-postcss'),
-  sass = require('gulp-sass'),
+  merge = require('gulp-merge-json'),
+  path = require('path'),
+  pify = require('pify'),
+  rename = require('gulp-rename'),
+  shell = require('shelljs'),
   source = require('vinyl-source-stream'),
   sourcemaps = require('gulp-sourcemaps'),
+  terser = require('gulp-terser-js'),
   watch = require('gulp-watch'),
-  rtlcss = require('gulp-rtlcss'),
-  rename = require('gulp-rename'),
-  merge = require('gulp-merge-json'),
-  endOfStream = require('end-of-stream')
+  watchify = require('watchify'),
+  zip = require('gulp-zip')
 
+const endOfStream = pify(require('end-of-stream'))
 
-const livereloadJson = require('./.config/gulplivereload.json')
-const projectJson = require('./.config/project.json')
+/* ==================== Global Constants Defined ======================  */
+
+const envArgs = dotenv.config({
+  path:path.resolve(process.cwd(),'.config/.env'),
+  encoding:'utf8'
+})
+
+if(envArgs.error){
+  throw envArgs.error
+}
+
+// env checked end
+console.log('CurrentBuildMode:[',NodeEnv(),'],Version:[',ExtVersion(),']')
+
+var dateFormat = new DateFormat('YYDDDD')
+if(isPreRelease()){
+  dateFormat = new DateFormat('MMDDDD-HHmm')
+}
+
+const liveOpts = {
+  port:36489
+}
 
 const gulpPaths = Object.assign({
-  TARGET:'firefox',
-  BUILD:'./build',
-  DEST:'./dist',
-  SRC:'src',
-  APP:"./addon",
+  APP:'addon',
+  BUILD:'build',
+  DEST:'dist',
+  CONFIG:'.config',
+  EXTFILE:'version-info.json'
 },projectJson)
 
 const browserPlatforms = [
-  'firefox',
-  'chromium',
+  'firefox'
 ]
 
-const commonPlatforms = [
-  ...browserPlatforms,
-]
-
-function comboTargetDest(){
-  return `${gulpPaths.DEST}/${gulpPaths.TARGET}/`
-}
-
-function comboTargetBuild(){
-  return `${gulpPaths.BUILD}/${gulpPaths.TARGET}/`
-}
-
-
-//Gulp Tasks
-const copyTaskNames = []
-const copyDevTaskNames = []
-
-var packagePathName = packageJson.name || "basexer"
-
-if(packageJson.version && packageJson.version.split('.').length > 0){
-  packagePathName = packagePathName +'_' + packageJson.version.split('.').join('_');
-}
-console.log(packagePathName)
-//add Task
-createCopyTasks('locales',{
-  source:`${gulpPaths.APP}/_locales/`,
-  destinations:commonPlatforms.map(platform => `${gulpPaths.BUILD}/${packagePathName}_${platform}/_locales`)
+/* ==================== Early Tasks Defined ======================  */
+gulp.task('clean',()=>{
+  return del([`${gulpPaths.build}/*`])
 })
 
-createCopyTasks('icons',{
-  source:`${gulpPaths.APP}/icons/`,
-  destinations:commonPlatforms.map(platform => `${gulpPaths.BUILD}/${packagePathName}_${platform}/icons`)
+gulp.task('dev:reload',()=>{
+  livereload.listen(liveOpts)
 })
 
-createCopyTasks('vendor',{
-  source:`${gulpPaths.APP}/vendor/`,
-  destinations:commonPlatforms.map(platform => `${gulpPaths.BUILD}/${packagePathName}_${platform}/vendor`)
+/* ----------------- Edit ExtInfo ------------------------ */
+gulp.task('set:extinfo',() =>{
+  const _src = ExtInfoFile()
+  const _dest = ExtInfoDest()
+
+  return gulp.src(_src)
+    .pipe(jsoneditor((json) =>{
+
+    }))
+    .pipe(rename('info.json'))
+    .pipe(gulp.dest(ExtInfoDest(),{overwrite:true}))
 })
 
-createCopyTasks('html',{
-  source:`${gulpPaths.APP}/`,
+function editExtInfo(json){
+  if(typeof json === undefined){
+    json = {}
+  }
+  json.version = ExtVersion()
+  json.buildTag = dateFormat.format(new Date())
+}
+/* ----------------- Edit ExtInfo ------------------------ */
+
+
+//Copy Tasks Begin
+const copyTasksNames = []
+const copyDevTasksNames = []
+
+createCopyTask('locales',{
+  source:GetCopySrc('_locales'),
+  pattern:'**/*.json',
+  destinations:BuildTargets('_locales')
+})
+
+createCopyTask('icons',{
+  source:GetCopySrc('icons'),
+  destinations:BuildTargets('icons')
+})
+
+createCopyTask('images',{
+  source:GetCopySrc('images'),
+  destinations:BuildTargets('images')
+})
+
+createCopyTask('css',{
+  source:GetCopySrc('css'),
+  destinations:BuildTargets('css')
+})
+
+createCopyTask('vendor',{
+  source:GetCopySrc('vendor'),
+  destinations:BuildTargets('vendor')
+})
+
+createCopyTask('html',{
+  source:GetCopySrc(),
   pattern:'**/*.html',
-  devMode:true,
-  destinations:commonPlatforms.map(platform => `${gulpPaths.BUILD}/${packagePathName}_${platform}`)
+  destinations:BuildTargets()
 })
 
-createCopyTasks('css',{
-  source:`${gulpPaths.APP}/css/`,
-  destinations:commonPlatforms.map(platform => `${gulpPaths.BUILD}/${packagePathName}_${platform}/css`)
-})
-
-createCopyTasks('js',{
-  source:`${gulpPaths.APP}/js/`,
-  pattern:'/**/*.js',
-  destinations:commonPlatforms.map(platform => `${gulpPaths.BUILD}/${packagePathName}_${platform}/js`)
-})
-
-
-
-//merge manifest json task
-createCopyMergeManifestTask(commonPlatforms)
-
-
-//execute task setting
-gulp.task('dev:copy',
-  gulp.series(
-    gulp.parallel(...copyDevTaskNames)
-  )
-)
-
-gulp.task('dev:reload',function() {
-  livereload.listen({"port":livereloadJson.livereloadOpts.port||39517})
-})
-
-gulp.task('dev:scss',createScssBuildTask({
-  src:`${gulpPaths.SRC}/scss/style.scss`,
-  dest:`${gulpPaths.APP}/css`,
-  pattern:`${gulpPaths.SRC}/scss/**/*.scss`
-}))
-
-/**
- *
- * @DateTime 2019-12-11
- * @param    string   label taskName
- * @param    Json Object   opts  [source,destination,destinations,pattern,devMode=false]
- * @return   gulp instance
- */
-function createCopyTasks (label, opts) {
-  if(!opts.devOnly) {
+function createCopyTask(label,opts){
+  if(typeof opts.devMode === undefined)opts.devMode = isDevMode()
+  if(!opts.devOnly){
     const copyTaskName = `copy:${label}`
     copyTask(copyTaskName,opts)
-    copyTaskNames.push(copyTaskName)
+    copyTasksNames.push(copyTaskName)
   }
+
   const copyDevTaskName = `dev:copy:${label}`
-  copyTask(copyDevTaskName,Object.assign({devMode:true},opts))
-  copyDevTaskNames.push(copyDevTaskName)
+  copyTask(copyDevTaskName,opts)
+
+  copyDevTasksNames.push(copyDevTaskName)
 }
 
 function copyTask(taskName,opts){
   const source = opts.source
   const destination = opts.destination
   const destinations = opts.destinations || [destination]
-  const pattern = opts.pattern ||'/**/*'
+  const pattern = opts.pattern || '**/*'
   const devMode = opts.devMode
-  //console.log('opts.devMode',opts.devMode)
-  return gulp.task(taskName,function() {
+
+  return gulp.task(taskName,()=>{
     if(devMode){
-//      watch(pattern,async (event) => {
-//        livereload.changed(event.path);
-//      })
+      watch(source+pattern, (event) => {
+        console.log('Copy watch',event.path)
+        livereload.changed(event.path)
+        performCopy()
+      })
     }
-    return preformCopy()
+
+    return performCopy()
   })
 
-  function preformCopy(){
-    let stream = gulp.src(source + pattern,{base:source})
+  function performCopy() {
+    let stream = gulp.src(source+pattern, { base : source })
 
-    //copy to destinations
-    destinations.forEach(function(dest) {
-      stream = stream.pipe(gulp.dest(dest))
+    destinations.forEach( dest => {
+      stream = stream.pipe(gulp.dest(dest,{overwrite:true}))
     })
 
     return stream
   }
 }
 
-/*gulp.task('update:version',function(){
-  return gulp.src(`${gulpPaths.SRC}/common.manifest.json`)
-    pipe(jsoneditor((json) =>{
-      if(packageJson.version)json.version = packageJson.version
-      return json
-    }))
-    .pipe(gulp.dest(`${gulpPaths.SRC}/common.manifest.json`,{overwrite:true}))
-})*/
+//Copy Tasks Begin
 
-function createCopyMergeManifestTask(platforms) {
-  let targets = platforms || commonPlatforms
-  targets.map(target => {
-    let mergeTaskName = `copy:merge:${target}`
-    let opts = {}
-    opts.src = `${gulpPaths.SRC}/${target}.manifest.json`
-    opts.dest = `${gulpPaths.BUILD}/${packagePathName}_${target}`
-    mergeManifestTask(mergeTaskName,opts);
-    copyTaskNames.push(mergeTaskName)
 
-    let devMergeTaskName = `dev:copy:merge:${target}`
-    mergeManifestTask(devMergeTaskName,opts);
-    copyDevTaskNames.push(devMergeTaskName)
-  })
+
+/* ==================== Latest Tasks Defined ======================  */
+gulp.task('default',gulp.series(TaskDefaultName()))
+
+
+const TaskDefaultName = () => {
+  return 'set:extinfo'
+}
+/* ==================== Common Methods Defined ======================  */
+const ExtInfoFile = () =>{
+  return `${gulpPaths.CONFIG}/${gulpPaths.EXTFILE}`
 }
 
-function mergeManifestTask(taskName,opts) {
-  const commonSrc = `${gulpPaths.SRC}/common.manifest.json`
-
-  return gulp.task(taskName,function(){
-    return gulp.src([
-      commonSrc,
-      opts.src
-    ]).pipe(merge())
-    .pipe(jsoneditor((json) =>{
-      //console.log('>>>',JSON.stringify(packageJson))
-      if(packageJson.version)json.version = packageJson.version
-      if(packageJson.author)json.author = packageJson.author
-      return json
-    }))
-    .pipe(rename('manifest.json'))
-    .pipe(gulp.dest(opts.dest,{overwrite:true}))
-  })
+const ExtInfoDest = () => {
+  return `${gulpPaths.SRC}/scripts/runtime/`
 }
 
-// copy merge manifest
-
-
-
-
-
-//['src/scss/**/*.scss']
-//
-function createScssBuildTask ({src,dest,devMode,pattern }){
-  return function(){
-    if(devMode) {
-      watch(pattern,async (event) =>{
-        const stream = buildScss()
-        await endOfStream(stream)
-        console.log(`${event.path}`,'changed')
-        livereload.changed(event.path);
-      })
-      return buildScssWithSourceMaps()
-    }
-    return buildScss()
-  }
-
-  function buildScssWithSourceMaps (){
-    return gulp.src(src)
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error',sass.logError))
-      .pipe(sourcemaps.write())
-      .pipe(autoprefixer())
-      .pipe(gulp.dest(dest))
-      .pipe(rtlcss())
-      .pipe(rename({suffix:'-rtl'}))
-      .pipe(gulp.dest(dest))
-  }
-
-  function buildScss() {
-    return gulp.src(src)
-      .pipe(sass().on('error',sass.logError))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest(dest))
-      .pipe(rtlcss())
-      .pipe(rename({suffix:'-rtl'}))
-      .pipe(gulp.dest(dest))
-  }
+const NodeEnv = ()=> {
+  return process.env.NODE_ENV ||'development'
 }
 
-function scssTask(){
-  return gulp
-    .src(`${gulpPaths.SRC}/scss/**/*.scss`)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .on('error',sass.logError)
-    .pipe(postcss([autoprefixer(),cssnano()]))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(comboTargetBuild()));
+const GetTarget = () => {
+  return process.env.DEST_TARGET || 'firefox'
 }
 
-
-//webext build sign
-// exports.webextfox =async function BuildFirefoxDestop(){
-//   return gulp.task('webext:fox',function(){
-//     webExt.cmd.build({
-//       overwriteDest: true,
-//       sourceDir:`${gulpPaths.BUILD}/${packagePathName}_${target}`,
-//       artifactsDir:`${gulpPaths.DEST}/${target}`
-//     },{
-//       shouldExitProgram: false
-//     }).then((extensionRunner) =>{
-//       console.log(extensionRunner);
-//     })
-//   })
-// }
-
-
-
-gulp.task('clean',function(){
-  return del(`${gulpPaths.BUILD}/`)
-})
-
-gulp.task('dev:extension',
-  gulp.series(
-    'clean',
-    'dev:scss',
-    gulp.parallel(
-      'dev:copy',
-    )
-  )
-)
-
-//{ start: true }
-//"dev:reload"
-
-
-
-exports.style = scssTask;
-
-// ============================
-// COMMON
-// ============================
-function gulpParallel(...args) {
-  return function spawnGulpChildProcess(cb) {
-    return gulpMultiProcess(args,cb,true)
-  }
+const GetAuthor = () => {
+  return process.env.EXT_AUTHOR || pkgJson.author || ''
 }
 
-/**
- * Default Gulp Task
- */
-gulp.task('default',gulp.series("dev:extension"));
+const isPreRelease = ()=>{
+  return NodeEnv() == 'development'
+}
+
+const isDevMode = ()=>{
+  return NodeEnv() == 'development'
+}
+
+const ExtName = () => {
+  return process.env.EXT_NAME || pkgJson.name
+}
+
+const ExtVersion = () => {
+  return process.env.EXT_VER || pkgJson.version
+}
+
+const GetCopySrc = (subPaths) => {
+  return subPaths ? `${gulpPaths.APP}/${subPaths}/` : `${gulpPaths.APP}/`
+}
+
+const BuildTargets = (subPaths) => {
+  let destTargets = [...browserPlatforms]
+  return destTargets.map(target => subPaths ? `${gulpPaths.BUILD}/${target}/${subPaths}` : `${gulpPaths.BUILD}/${target}/`)
+}

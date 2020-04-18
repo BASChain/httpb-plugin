83dd65962119d83ce837d87f70b3fd7ba3ea0c46
developer <team-developer@gmail.com>
2020-04-18 08:36:04 +0800

    add network switch

--- addon/js/popup.js
+++ addon/js/popup.js
@@ -7,7 +7,7 @@
  *
  * Copyright (c) 2019 BAS,bas-devteam@gmail.com
  * E-mail :bas-ext@gmail.com
- * git@flash:BASChain/httpb-plugin.git
+ * git@bas:BASChain/httpb-plugin.git
  *
  */
 'use strict'
@@ -15,10 +15,45 @@ angular.module('popupApp', []).controller('PopupCtrl', ['$scope', function($s) {
   var storage = chrome.storage.local;
   $s.extVersion = getVersion()
   $s.extName = getName()
+  $s.networks = getNetworks()
+  //$s.chainId = $s.networks[0].value
+
+  storage.get({chainId:''},function(obj){
+    console.log('get from storage',obj)
+    $s.chainId = obj.chainId || $s.networks[0].value
+    $s.$apply()
+  })
+
+  // if(!$s.chainId){
+  //   $s.chainId = 3;
+  //   storage.set({chainId:3})
+  // }
+
+  //$s.chainId = 3;
+
+  $s.networkChange = function() {
+    const network = $s.chainId
+    console.log('Current:',network)
+    //p3ctx.Storage.set('chainId',network)
+    storage.get({chainId:network},function(obj){
+      storage.set({chainId:network})
+      $s.chainId = network;
+      $s.$apply()
+    })
+  }
+  //const chainId = p3ctx.Storage.get('chainId') ||$s.networks[1].value
+  //$s.selectChainId = chainId
+
 }]);
+function getNetworks(){
+  return [
+    {value:1,name:"Mainnet",i18n:"mainnet"},
+    {value:3,name:"Ropsten",i18n:"ropsten"}
+  ]
+}
 
 function getVersion(){
-  let v = '0.0.5';
+  let v = '0.1.1';
   if(browser&&browser.runtime){
     v = browser.runtime.getManifest().version
   }
--- src/scripts/p3lib.js
+++ src/scripts/p3lib.js
@@ -3,14 +3,25 @@ const browser = require('webextension-polyfill')
 global.$ = global.jQuery = require('jquery')
 
 const { Info , BrowerInfo , BindThisProperties } = require('./runtime')
+const {
+  Networks,
+  getNetwork,
+  StorageHelper
+} = require('./utils')
 
 class P3Ctx {
   constructor(browser){
     BindThisProperties.call(this,Info)
     this.BwInfo = new BrowerInfo()
     this.runtime = browser.runtime
+    this.Storage = StorageHelper
+    this.networks = Networks
     console.log('load P3');
+  }
+
+  network(chainId){
+    return getNetwork(chainId)
   }
 }
 
-global.pInst = 'oooo'
+global.p3ctx = new P3Ctx(window.browser||'')
